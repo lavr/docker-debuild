8e16b8b7892305c70057f81e48c30f755e234aef
Sergey Lavrinenko <s@lavr.me>
2016-11-14 05:31:32 +0300

    fix result files permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,14 @@ cd /target || exit 1
 
 apt-get update
 
+USER_ID=${LOCAL_USER_ID:0}
+
 for i; do (
 	set -e
 	cd $i
 	mk-build-deps --install --remove --tool "apt-get --no-install-recommends --yes"
 	eval "debuild $DOCKER_DEBUILD_OPTS -us -uc --lintian-opts --allow-root"
+        # Fix permission
+        chown -R $USER_ID .
 )
 done
