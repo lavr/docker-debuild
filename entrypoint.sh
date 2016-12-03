ee5e7a2ad312462d9c09dc7a45ae29ee3b37794f
Sergey Lavrinenko <s@lavr.me>
2016-12-04 02:28:16 +0300

    Fix permission fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,18 @@ cd /target || exit 1
 
 apt-get update
 
-USER_ID=${LOCAL_USER_ID:0}
-
 for i; do (
 	set -e
 	cd $i
 	mk-build-deps --install --remove --tool "apt-get --no-install-recommends --yes"
 	eval "debuild $DOCKER_DEBUILD_OPTS -us -uc --lintian-opts --allow-root"
-        # Fix permission
-        chown -R $USER_ID .
+
+        if [ -z "$LOCAL_USER_ID" ]
+        then
+            # do nothing
+        else
+            # Fix permission
+            chown -R $LOCAL_USER_ID .
+        fi
 )
 done
